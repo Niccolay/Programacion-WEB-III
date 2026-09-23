9f9accb3e93874b9fdb884e655148388e4205dc8
Niccolay <aphelphelios@gmail.com>
2026-09-23 16:08:15 -0400

    🎉 first commit

---
+++ Frontent/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+
+    return <>
+        <div className="w-full flex justify-center">
+            <img className="object-cover h-dvh" src="https://raw.githubusercontent.com/APHELOS/cat/refs/heads/main/img/cat.jpg"/>
+        </div>
+    </>
+}
+
+export default App;
