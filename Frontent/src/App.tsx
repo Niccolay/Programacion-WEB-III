3a6f9a15282b7b608bd043cb8a4fd017767577db
Niccolay <aphelphelios@gmail.com>
2026-09-23 16:16:05 -0400

    ...

--- Frontent/src/App.tsx
+++ Frontent/src/App.tsx
@@ -3,8 +3,12 @@ import "./App.css";
 function App() {
 
     return <>
-        <div className="w-full flex justify-center">
-            <img className="object-cover h-dvh" src="https://raw.githubusercontent.com/APHELOS/cat/refs/heads/main/img/cat.jpg"/>
+        <div className="w-full relative flex justify-center">
+            <img className="object-cover h-dvh" src="https://raw.githubusercontent.com/APHELOS/cat/refs/heads/main/img/cat.jpg" />
+            <div className="absolute top-10 -left-60">
+                <h2>Programacion web-III</h2>
+
+            </div>
         </div>
     </>
 }
